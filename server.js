2474b05d94d84c53c2f9d016df9e432b2871625e
Edward Lanto <elanto@hotmail.com>
2020-09-04 15:50:38 -0700

    scaffold node js server, built room view and attached uuid

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const { v4: uuidv4 } = require('uuid');
+
+// renders ejs file.
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+   res.redirect(`/${uuidv4()}`);  
+});
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room})
+})
+
+server.listen(5000);
