ae6d59f191bc7abd3b7ff8398aee32ebc2353b5e
Edward Lanto <elanto@hotmail.com>
2020-09-04 18:23:12 -0700

    installed socket io, set video stream

--- public/script.js
+++ public/script.js
@@ -1,4 +1,27 @@
+
+const videoGrid = document.getElementById('video-grid');
+const myVideo = document.createElement('video');
+const socket = io('/')
+myVideo.muted = true;
+
+// Declare variable to set video stream value on promise res
+let videoStream;
+
 navigator.mediaDevices.getUserMedia({
     video: true,
-    audio
+    audio: true
+}).then(stream => {
+    videoStream = stream;
+    addVideoStream(videoStream, stream)
 });
+
+socket.emit('join-room');
+
+const addVideoStream = (video, stream) => {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    });
+    videoGrid.append(video);
+}
+
--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const { v4: uuidv4 } = require('uuid');
+const io = require('socket.io')(server);
 
 // renders ejs file.
 app.set('view engine', 'ejs');
+
+// Tell server to grab where the javascript files are going to live
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
    res.redirect(`/${uuidv4()}`);  
@@ -12,6 +16,16 @@ app.get('/', (req, res) => {
 
 app.get('/:room', (req, res) => {
     res.render('room', { roomId: req.params.room})
+});
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId) => {
+        console.log('room id', roomId);
+        socket.join(roomId);
+
+        // Broadcasts to other user instances that a user has joined.
+        socket.to(roomId).broadcoast.emit('user-connected');
+    })
 })
 
 server.listen(5000);
